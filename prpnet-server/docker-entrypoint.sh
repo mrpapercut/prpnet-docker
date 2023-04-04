5443a3e3c10cb79ed0ae485493cd1689b98539e8
Mischa Rodermond <m.rodermond@nfir.nl>
2023-04-04 12:39:05 +0200

    Adding prpnet-server setup files

--- prpnet-server/docker-entrypoint.sh
+++ prpnet-server/docker-entrypoint.sh
@@ -3,6 +3,9 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
+envsubst < database.conf > database.ini
+envsubst < prpserver.conf > prpserver.ini
+
 # Wait for the backend to be up, if we know where it is.
 ./waitforit.sh "mysql:3306"
 
